5582ef7440b90aadf36771bf64c710209d05fa49
sumaiya719 <schowdhury719@gmail.com>
2015-09-16 21:27:36 +0600

    (sumaiya)TaskGroupService and TaskGroupServiceImpl added

---
+++ src/main/java/com/great/cms/service/TaskGroupService.java
@@ -0,0 +1,17 @@
+package com.great.cms.service;
+
+
+import java.util.List;
+
+import com.great.cms.db.entity.Student;
+import com.great.cms.db.entity.TaskProject;
+import com.great.cms.db.entity.Groups;
+import com.great.cms.db.entity.Task;
+
+public interface TaskGroupService {
+	public List<Groups> findGroupsByTaskID(int taskId);
+	public void addNewGroupOfTask(Task taskId,String groupName, List<Student> student);
+	
+	
+		
+}
